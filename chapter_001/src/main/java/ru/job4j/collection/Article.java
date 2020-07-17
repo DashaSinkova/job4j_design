c09bbc97c104e7f06713be3d3ef6d1d3d2fc84cb
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-07-17 16:07:01 +0300

    1. Генератор текста

--- chapter_001/src/main/java/ru/job4j/collection/Article.java
+++ chapter_001/src/main/java/ru/job4j/collection/Article.java
@@ -1,12 +1,16 @@
 package ru.job4j.collection;
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String[] strings = line.split(" ");
         boolean res = true;
-        for (String string : strings) {
-            if (!origin.contains(string)) {
+        String[] stringOrigin = origin.replaceAll("\\p{Punct}", "").split(" ");
+       HashMap<String, String> origins = new HashMap<>();
+        origins.putAll(Arrays.stream(stringOrigin).collect(Collectors.toMap(el -> el, el -> el, (first, second) -> first)));
+        String[] stringLine = line.split(" ");
+        for (String str : stringLine) {
+            if (!origins.containsKey(str)) {
                 res = false;
                 break;
             }
