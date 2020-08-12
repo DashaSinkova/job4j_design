e97c5f9f1bd120df5eff03a49d708f82dba1f4da
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-08-12 16:00:46 +0300

    0.3. BufferedReader. [#297684]

--- chapter_001/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_001/src/main/java/ru/job4j/io/LogFilter.java
@@ -7,12 +7,21 @@ import java.util.List;
 public class LogFilter {
     public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines().filter(el -> el.contains("404")).forEach(list :: add);
+            in.lines().forEach(list :: add);
+            for (String line : list) {
+                if (line.contains("404")) {
+                    String[] arr = line.split(" ");
+                    if (arr[arr.length - 2].equals("404")) {
+                        res.add(line);
+                    }
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
+        return res;
     }
 
     public static void main(String[] args) {
