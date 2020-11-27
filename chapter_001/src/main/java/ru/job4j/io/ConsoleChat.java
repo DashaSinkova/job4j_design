eb0dcc3b50510402b5745c93a52ceab435f08a0d
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-11-27 19:51:05 +0300

    6. Кодировка. [#297691]

--- chapter_001/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_001/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,4 +1,78 @@
+/*
+Курс Петра/ Джуниор/  Блок 2.2. Ввод-вывод/ 1. Ввод-вывод/ 6. Кодировка. [#297691]
+'Консольный чат'
+ */
 package ru.job4j.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+          Scanner scanner = new Scanner(System.in);
+        System.out.print("Пользователь: ");
+        String line = scanner.nextLine();
+        boolean isBotAnswer = true;
+        while (!line.equals(OUT)) {
+            if (line.equals(STOP)) {
+                isBotAnswer = false;
+            } else if (line.equals(CONTINUE)) {
+                isBotAnswer = true;
+            }
+            if (isBotAnswer) {
+            //ищем ответ из другого файла, прибавляем к строке
+                String botAnswer = searchAnswer();
+                line = "Пользователь: " + line + System.lineSeparator() + "Бот: " + botAnswer;
+                System.out.println("Бот: " + botAnswer);
+
+            } else {
+                line = "Пользователь: " + line;
+            }
+            //записываем строку в итоговый файл
+            writeInFile(line);
+            System.out.print("Пользователь: ");
+            line = scanner.nextLine();
+        }
+        System.out.println("Бот: Разговор окончен.");
+        writeInFile("Пользователь: " + line + System.lineSeparator() + "Бот: Разговор окончен.");
+    }
+    private void writeInFile(String line) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+            writer.write(line + System.lineSeparator());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private String searchAnswer() {
+        String res = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(botAnswers))) {
+            Object[] array = reader.lines().toArray();
+            double i = array.length * Math.random();
+            int index = (int) i;
+            res = array[index].toString();
+        } catch (Exception e) {
+            System.out.println("Файл пуст");
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("./path.txt", "./botanswers.txt");
+        cc.run();
+    }
 }
--- chapter_001/src/main/java/ru/job4j/io/UsageEncoding.java
+++ chapter_001/src/main/java/ru/job4j/io/UsageEncoding.java
@@ -1,4 +1,47 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.charset.Charset;
+import java.util.List;
+
 public class UsageEncoding {
+    public String readFile(String path) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path, Charset.forName("WINDOWS-1251")))) {
+            int read;
+            while ((read = reader.read()) > 0) {
+                builder.append((char) read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public void writeInFile(String path, String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            writer.write(data + System.lineSeparator());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "./text.txt";
+        UsageEncoding encoding = new UsageEncoding();
+        List<String> strings = List.of(
+                "Новая строка 1",
+                "Новая строка 2",
+                "Новая строка 3",
+                "Новая строка 4",
+                "Новая строка 5"
+        );
+        for (String s : strings) {
+            encoding.writeInFile(path, s);
+        }
+        System.out.println(encoding.readFile(path));
+    }
 }
