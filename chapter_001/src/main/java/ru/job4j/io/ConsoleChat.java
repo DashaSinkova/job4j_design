8f55506d2b66a142c8b32d44e3ed7159197c76f9
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-11-30 20:58:03 +0300

    6. Кодировка. [#297691]

--- chapter_001/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_001/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,18 +1,23 @@
 /*
 Курс Петра/ Джуниор/  Блок 2.2. Ввод-вывод/ 1. Ввод-вывод/ 6. Кодировка. [#297691]
 'Консольный чат'
- */
+*/
 package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ConsoleChat {
     private final String path;
     private final String botAnswers;
+    private List<String> answers;
+    private List<String> dialog = new ArrayList<>();
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
@@ -23,7 +28,16 @@ public class ConsoleChat {
     }
 
     public void run() {
-          Scanner scanner = new Scanner(System.in);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(botAnswers))) {
+            answers = bufferedReader.lines().collect(Collectors.toList());
+            if (answers.size() == 0) {
+                throw new Exception("Файл пуст");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Пользователь: ");
         String line = scanner.nextLine();
         boolean isBotAnswer = true;
@@ -35,7 +49,7 @@ public class ConsoleChat {
             }
             if (isBotAnswer) {
             //ищем ответ из другого файла, прибавляем к строке
-                String botAnswer = searchAnswer();
+                String botAnswer = answers.get((int) (answers.size() * Math.random()));
                 line = "Пользователь: " + line + System.lineSeparator() + "Бот: " + botAnswer;
                 System.out.println("Бот: " + botAnswer);
 
@@ -43,32 +57,23 @@ public class ConsoleChat {
                 line = "Пользователь: " + line;
             }
             //записываем строку в итоговый файл
-            writeInFile(line);
+            dialog.add(line);
             System.out.print("Пользователь: ");
             line = scanner.nextLine();
         }
         System.out.println("Бот: Разговор окончен.");
-        writeInFile("Пользователь: " + line + System.lineSeparator() + "Бот: Разговор окончен.");
+        dialog.add("Пользователь: " + line + System.lineSeparator() + "Бот: Разговор окончен.");
+        writeInFile(dialog);
     }
-    private void writeInFile(String line) {
+
+    private void writeInFile(List<String> dialog) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
-            writer.write(line + System.lineSeparator());
+            for (String line : dialog) {
+                writer.write(line + System.lineSeparator());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    private String searchAnswer() {
-        String res = "";
-        try (BufferedReader reader = new BufferedReader(new FileReader(botAnswers))) {
-            Object[] array = reader.lines().toArray();
-            double i = array.length * Math.random();
-            int index = (int) i;
-            res = array[index].toString();
-        } catch (Exception e) {
-            System.out.println("Файл пуст");
-            e.printStackTrace();
-        }
-        return res;
     }
 
     public static void main(String[] args) {
