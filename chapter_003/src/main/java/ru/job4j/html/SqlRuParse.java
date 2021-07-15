10b425964f7c0af4943fed34dbd7d3816e2dedf4
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2021-07-15 23:40:11 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ chapter_003/src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,24 @@
+package ru.job4j.html;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");//полечаем нужный тег
+        int count = 0;
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(doc.getElementsByAttribute("style").select(".altCol").get(count).text());
+            count ++;
+            System.out.println();
+        }
+    }
+}
+
